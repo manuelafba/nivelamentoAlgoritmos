6f81d9cf2ad54ec4e68226269181d234d731f8d1
Manuela Araújo <104527846+manuelafba@users.noreply.github.com>
2024-02-15 18:48:07 +0000

    exemplos aula 4

---
+++ aula 4/exemplos/exemplo01.py
@@ -0,0 +1,5 @@
+frutas = ["Morango", "Banana", "Uva", "Maracujá"]
+print(frutas[3])
+
+notas = (9.5, 8.0, 6.5, 7.0)
+print(notas[2])
---
+++ aula 4/exemplos/exemplo02.py
@@ -0,0 +1,7 @@
+notas = [8.0, 5.5, 1.5]
+for item in notas:
+    print(item)
+
+cores = ("vermelho", "azul", "roxo")
+for item in cores:
+    print(item)
