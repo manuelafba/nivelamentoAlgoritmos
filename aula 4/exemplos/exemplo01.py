399e3f99661c7145b173df037d88106be3d6230d
Manuela Araújo <mxnuelafba@gmail.com>
2024-02-16 16:37:25 -0300

    aula 1 adicionada

--- aula 4/exemplos/exemplo01.py
+++ aula 4/exemplos/exemplo01.py
@@ -1,5 +1,5 @@
 frutas = ["Morango", "Banana", "Uva", "Maracujá"]
-print(frutas[3])
+print(frutas[3]) # Exibir o elemento no índice 3
 
 notas = (9.5, 8.0, 6.5, 7.0)
-print(notas[2])
+print(notas[2]) # Exibir o elemento no índice 2
--- aula 4/exemplos/exemplo02.py
+++ aula 4/exemplos/exemplo02.py
@@ -1,7 +1,7 @@
 notas = [8.0, 5.5, 1.5]
-for item in notas:
+for item in notas: # Iteração por cada item na lista "notas"
     print(item)
 
 cores = ("vermelho", "azul", "roxo")
-for item in cores:
+for item in cores: # Iteração por cada item na tupla "cores"
     print(item)
