a1d98c25ff6f2e5282310852b79a7be49cacc616
Manuela Araújo <mxnuelafba@gmail.com>
2024-02-16 16:39:48 -0300

    movendo pastas

--- aula 4/exemplos/exemplo01.py
+++
@@ -1,5 +0,0 @@
-frutas = ["Morango", "Banana", "Uva", "Maracujá"]
-print(frutas[3]) # Exibir o elemento no índice 3
-
-notas = (9.5, 8.0, 6.5, 7.0)
-print(notas[2]) # Exibir o elemento no índice 2
--- aula 4/exemplos/exemplo02.py
+++
@@ -1,7 +0,0 @@
-notas = [8.0, 5.5, 1.5]
-for item in notas: # Iteração por cada item na lista "notas"
-    print(item)
-
-cores = ("vermelho", "azul", "roxo")
-for item in cores: # Iteração por cada item na tupla "cores"
-    print(item)
