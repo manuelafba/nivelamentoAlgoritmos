7574edd14c5de17463e701d5e940a25c819edb6c
Manuela Araújo <104527846+manuelafba@users.noreply.github.com>
2024-02-19 18:37:40 -0300

    comentários adicionados

--- aula 1/exercícios/ex03.py
+++ aula 1/exercícios/ex03.py
@@ -1,9 +1,9 @@
 a = int(input("Digite o valor de A: "))
 b = int(input("Digite o valor de B: "))
 
-c = a
-a = b
-b = c
+auxiliar = a # O valor de A é guardado na variável auxiliar
+a = b # O valor de B é armazenado na variável A
+b = auxiliar # A variável B recebe o valor das variável auxiliar, que é o valor incial de A
 
 print(a)
 print(b)
