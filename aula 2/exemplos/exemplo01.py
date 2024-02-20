ee44d7ed4679ac142016e062440b722f78b3a6a2
Manuela Araújo <104527846+manuelafba@users.noreply.github.com>
2024-02-20 20:08:17 -0300

    Update exemplo01.py

--- aula 2/exemplos/exemplo01.py
+++ aula 2/exemplos/exemplo01.py
@@ -10,5 +10,5 @@ y = a > 0 or b < 0 # Verdadeiro, pois pelo menos um dos números é maior que ze
 print(y)
 
 # Operador NOT
-z = not x == 0 # A afirmação "x == 0" é falsa, pois x é diferente de zero. Porém, o operador NOT inverte o seu valor lógico, retornando TRUE 
+z = not a == 0 # A afirmação "a == 0" é falsa, pois a é diferente de zero. Porém, o operador NOT inverte o seu valor lógico, retornando TRUE 
 print(z)
