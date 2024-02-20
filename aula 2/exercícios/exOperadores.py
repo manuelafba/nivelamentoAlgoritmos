4d20f7efcc504453eb051806be6221d2a5e8c256
Manuela Araújo <mxnuelafba@gmail.com>
2024-02-20 18:42:12 -0300

    exercício operadores

---
+++ aula 2/exercícios/exOperadores.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+
+print(f"Número: {numero}")
+print(f"Antecessor: {numero - 1}")
+print(f"Sucessor: {numero + 1}")
+print(f"Dobro: {numero * 2}")
+print(f"Triplo: {numero * 3}")
+print(f"Quadrado: {numero ** 2}")
